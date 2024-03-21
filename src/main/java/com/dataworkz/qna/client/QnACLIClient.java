8919a41abf38fa95c7eb501e4e8b5a90784961de
sachin <sachin@arshna.com>
2024-03-21 14:23:38 +0530

    Supporting valid json output when input file is chosen

--- src/main/java/com/dataworkz/qna/client/QnACLIClient.java
+++ src/main/java/com/dataworkz/qna/client/QnACLIClient.java
@@ -346,7 +346,8 @@ abstract class BaseQnAClient implements Callable<Integer> {
                     writeToOutput("[\n");
                 });
             }
-            for (String s : inputs) {
+            for (int idx = 0; idx < inputs.size(); idx++) {
+                String s = inputs.get(idx);
                 if (s.startsWith("# ")) {
                     System.out.printf(s);
                     continue;
@@ -358,9 +359,11 @@ abstract class BaseQnAClient implements Callable<Integer> {
                 System.out.println("... Done. Took " + (System.currentTimeMillis() - time) + " msecs");
                 outputResponse(response);
                 if (format.equals("json")) {
-                    doIfOptionPresent(outputFile, () -> {
-                        writeToOutput(",\n");
-                    });
+                    if (idx + 1 < inputs.size()) {
+                        doIfOptionPresent(outputFile, () -> {
+                            writeToOutput(",\n");
+                        });
+                    }
                 }
                 try {
                     Thread.sleep(secondsBetweenQueries * 1000);
